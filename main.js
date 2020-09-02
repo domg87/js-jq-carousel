bc12d94810cd70d2be42422ce91443a9afb22714
dom <domepiro87@yahoo.it>
2020-09-02 17:38:42 +0200

    next_prec

---
+++ main.js
@@ -0,0 +1,35 @@
+
+$(document).ready(function(){
+    
+    $(".next").click(
+        function(){
+            var imgActive = $("img.active");
+            imgActive.removeClass("active");
+
+            if(imgActive.hasClass("last") == true){
+                var nextImg = $("img.first");
+            } else {
+                var nextImg = imgActive.next();
+            }
+            nextImg.addClass("active");
+        }
+    );
+
+    $(".prev").click(
+        function(){
+            var imgActive = $("img.active");
+            imgActive.removeClass("active");
+
+            if(imgActive.hasClass("first") == true){
+                var nextImg = $("img.first");
+            } else {
+                var nextImg = imgActive.prev();
+            }
+            nextImg.addClass("active");
+        }
+    );   
+
+
+
+
+});
